6c3cfa98ad113ce5c4dec4bd22151c6121909a76
Felipe Menino <felipe.carlos@fatec.sp.gov.br>
2020-03-29 22:43:27 -0300

    add unit test
    add unit test for some functions

--- geometry-algorithm/geometry-algorithm/main.cpp
+++ geometry-algorithm/geometry-algorithm/main.cpp
@@ -5,12 +5,5 @@
 
 int main()
 {
-	Point p1(2, 2);
-	Point p2(4, 4);
-	Point p3(3, 3);
-
-	std::cout << PointIsInSegment(p3, p1, p2) << std::endl;
-	std::cout << PointIsOverSegmentPoint(p1, p1, p2) << std::endl;
-
 	return 0;
 }
